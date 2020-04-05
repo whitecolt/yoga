a82f133edc7f0a15b2be4fa8e26b733b74cb0a32
whitecolt <whitecolt8@gmail.com>
2020-04-05 21:18:55 +0300

    created a timer

--- js/script.js
+++ js/script.js
@@ -8,31 +8,83 @@ window.addEventListener('DOMContentLoaded', function () {
 
 
   // Скроем весь контент табов на странице, оставив только первый таб
-  function hideContent(a) {
+  function hideTabContent(a) {
     for (let i = a; i < tabContent.length; i++) {
       tabContent[i].style.display = 'none';
     }
   }
 
-  hideContent(1);
+  hideTabContent(1);
 
   // Напишем функцию, которая будет показывать какой-то необходимый нам таб
-  function showContent(b) {
+  function showTabContent(b) {
     tabContent[b].style.display = 'flex';
   }
 
   // Используем делегирование для того, чтобы при клике на название таба нам показывался его контент
   info.addEventListener('click', function (event) {
     let target = event.target;
-    if (target && target.classList.contains('info-header-tab')) {
+    if (target) {
       for (let i = 0; i < tab.length; i++) {
-        if (target == tab[i]) {
-          hideContent(0);
-          showContent(i);
-          break;
+        if (tab[i] == target) {
+          hideTabContent(0);
+          showTabContent(i);
         }
       }
     }
   });
 
+  // Делаем интерактивный таймер
+
+
+  // Установим дату дедлайна
+  let deadline = '2020-05-01';
+
+
+  // получим время, оставшееся до дедлайна
+  function getTimeRemaining(endtime){
+    let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor(((t/1000) % 60)),
+        minutes = Math.floor((t/(1000*60)) % 60),
+        hours = Math.floor(t/(1000*60*60));
+
+// вернем объект, чтобы данными можно было пользоваться вне функции
+    return {
+      'total' : t,
+      'hours' : hours,
+      'minutes' : minutes,
+      'seconds' : seconds
+    };
+    
+  }
+
+
+// напишем функцию, которая получает элементы со страницы
+ function setClock(id, endtime){
+  let timer = document.getElementById(id),
+      hours = timer.querySelector('.hours'),
+      minutes = timer.querySelector('.minutes'),
+      seconds = timer.querySelector('.seconds'),
+      timeInterval = setInterval(updateClock, 1000);
+// напишем функцию, которая будет получать значения таймера в реальном времени
+    function updateClock(){
+      let t = getTimeRemaining(endtime);
+      hours.textContent = t.hours;
+      minutes.textContent = t.minutes;
+      seconds.textContent = t.seconds;
+
+      if (t.total < 0) {
+        clearInterval(timeInterval);
+      }
+      
+    }
+ };
+
+ setClock('timer', deadline);
+ 
 });
+
+
+
+
+
