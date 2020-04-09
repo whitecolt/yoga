070c26e729486d3cbe348a1aefcc0eecdc04c2fe
whitecolt <whitecolt8@gmail.com>
2020-04-09 20:18:27 +0300

    added modal window

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
 
   //  Установим дату дедлайна
-  let deadline = '2020-04-01';  
+  let deadline = '2020-05-01';  
 
 
   // получим время, оставшееся до дедлайна
@@ -94,7 +94,21 @@ window.addEventListener('DOMContentLoaded', function () {
  setClock('timer', deadline);
 });
 
+// Добавим модальное окно
 
+let more = document.querySelector('.more'),
+    overlay = document.querySelector('.overlay'),
+    close = document.querySelector('.popup-close');
 
+  more.addEventListener('click', function(){
+    overlay.style.display = 'block';
+    this.classList.add('more-splash');
+    document.body.style.overflow = 'hidden';
+  });
+
+  close.addEventListener('click', function(){
+    overlay.style.display = 'none';
+    more.classList.remove('.more-splash');
+  })
 
 
